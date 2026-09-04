ed589fae37120974a0fe2be13dcb4958dc67315f
praveen <praveenberlin007@gmail.com>
2026-09-04 07:36:03 +0530

    main

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from app import create_app
 
 app = create_app()
 
+#run
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5050))
     debug = os.environ.get('FLASK_DEBUG', '1') == '1'
